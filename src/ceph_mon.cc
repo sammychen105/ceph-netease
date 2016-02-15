81cdbd25e35f3ea8d397b6176a196b00d6682624
chenju <chenju@ubuntu.(none)>
2016-02-15 01:51:29 -0800

    test

--- src/ceph_mon.cc
+++ src/ceph_mon.cc
@@ -9,7 +9,7 @@
  * modify it under the terms of the GNU Lesser General Public
  * License version 2.1, as published by the Free Software 
  * Foundation.  See file COPYING.
- * 
+ *  
  */
 
 #include <sys/types.h>
